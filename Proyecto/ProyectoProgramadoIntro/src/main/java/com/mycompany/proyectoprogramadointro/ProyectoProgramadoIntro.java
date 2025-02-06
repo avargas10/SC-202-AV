e6ec88eecce66ed536f47b137386fde30b849f28
Andres Vargas <afelipe.vargas.r@gmail.com>
2025-02-06 17:43:46 -0600

    Commit inicial de proyecto

---
+++ Proyecto/ProyectoProgramadoIntro/src/main/java/com/mycompany/proyectoprogramadointro/ProyectoProgramadoIntro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoprogramadointro;
+
+/**
+ *
+ * @author andresvargasrivera
+ */
+public class ProyectoProgramadoIntro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
